9186fda3d626b0ea18864cc373d8b41fe90690e1
Aknandan Sharma <90132050+aknandansharma@users.noreply.github.com>
2023-02-27 20:35:27 +0530

    post

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/', (req, res) => {
 })
 
 // static files
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, './client/build')))
 
 app.get('*', function(req, res){
     res.sendFile(path.join(__dirname, './client/build/index.html'))
