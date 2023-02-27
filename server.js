412f92312a59166689e88727f35d697b327bcee7
Aknandan Sharma <90132050+aknandansharma@users.noreply.github.com>
2023-02-27 21:05:06 +0530

    updadha

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import categoryRoute from './routes/categoryRoute.js'
 import productRoutes from './routes/productRoutes.js'
 import cors from 'cors'
 import path from 'path'
-import { dirname } from 'path';
+import { fileURLToPath } from 'url;
 
 
 const app = express()
@@ -18,6 +18,10 @@ dotenv.config()
 
 // database config
 connectDB(); 
+
+// es module fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // middelwares
 app.use(cors())
